3e889ec0325893bba178b2a92e2259b03dbaa142
devMaxMayer <dev.maxmayer@gmail.com>
2021-02-02 15:00:08 +0400

    Initial commit

---
+++ src/main/java/com/university/studbook/StudbookApplication.java
@@ -0,0 +1,13 @@
+package com.university.studbook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudbookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudbookApplication.class, args);
+	}
+
+}
